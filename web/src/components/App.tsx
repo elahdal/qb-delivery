d0dce810e2e6082453a95fbd891d1df40a3855dd
melahdal <elahdalmohammed@gmail.com>
2025-02-16 00:02:06 +0100

    ui 60%

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -11,7 +11,8 @@ import logoPostal from "../images/image copy.png"
 import LogoPostOp from "../images/image copy 4.png"
 import Gotext from "../images/image copy 2.png"
 import PostOpText from "../images/image copy 5.png"
-
+import profilepic from "../images/image copy 6.png"
+import logoGoMini from "../images/image copy 7.png"
 
 // This will set the NUI to visible if we are
 // developing in browser
@@ -27,6 +28,8 @@ interface Player {
   id: number;
   playerState: string;
   time: string;
+  image: string;
+  image2: string;
 }
 
 interface PlayerContainerProps {
@@ -34,12 +37,12 @@ interface PlayerContainerProps {
 }
 
 const players: Player[] = [
-  { id: 1, playerState: "AFK", time: "01:22:20" },
-  { id: 2, playerState: "Ready", time: "01:22:20" },
-  { id: 3, playerState: "Ready", time: "01:22:20" },
-  { id: 3, playerState: "Ready", time: "01:22:20" },
-  { id: 3, playerState: "Ready", time: "01:22:20" },
-  { id: 3, playerState: "Ready", time: "01:22:20" },
+  { id: 1, playerState: "AFK", time: "01:22:20", image: profilepic, image2: logoGoMini },
+  { id: 2, playerState: "Ready", time: "01:22:20" , image: profilepic, image2: logoGoMini  },
+  { id: 3, playerState: "Ready", time: "01:22:20", image: profilepic, image2: logoGoMini  },
+  { id: 3, playerState: "Ready", time: "01:22:20" , image: profilepic, image2: logoGoMini },
+  { id: 3, playerState: "Ready", time: "01:22:20", image: profilepic, image2: logoGoMini  },
+  { id: 3, playerState: "Ready", time: "01:22:20" , image: profilepic, image2: logoGoMini },
 ];
 
 const PlayerContainer: React.FC<PlayerContainerProps> = ({ player }) => (
@@ -51,17 +54,18 @@ const PlayerContainer: React.FC<PlayerContainerProps> = ({ player }) => (
       <svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M10.2335 0C14.8293 0 17.1457 5.57997 13.8929 8.83119C10.6405 12.0821 5.05772 9.76697 5.05772 5.17326C5.05772 2.31606 7.37502 0 10.2335 0ZM23.2262 24.1679H20.4669V26.926H18.561V24.1679H15.8017V22.2632H18.561V19.5051H20.4666V22.2632H23.2259L23.2262 24.1679ZM18.5588 17.3195L20.457 17.2377C20.2969 13.7167 17.3694 10.9069 13.8332 10.9069H6.63346C2.98297 10.9069 0 13.8889 0 17.5372V21.0855C0 22.7827 1.38555 24.1679 3.08395 24.1679H13.5742V22.2632H3.08395C2.43779 22.2632 1.90563 21.7311 1.90563 21.0855V17.5372C1.90563 14.9402 4.03521 12.8113 6.63346 12.8113H13.8332C16.3558 12.8113 18.4454 14.8157 18.5588 17.3195ZM12.5455 2.86216C10.4909 0.808467 6.96335 2.27021 6.96335 5.17326C6.96335 8.07631 10.4912 9.53806 12.5455 7.48436C13.8226 6.20816 13.8226 4.13836 12.5455 2.86216Z" fill="white" fillOpacity="0.7"/></svg>
     </button>
     <div className="playertime">{player.time}</div>
-  </div>
-);
-
-interface PlayerStatsProps {
-  player: Player;
-}
-
-const PlayerStats: React.FC<PlayerStatsProps> = ({player}) =>(
-  <div className="playerstats">
-    <div className="playerstatsname">{player.playerState}</div>
-    <div className="playerstatstime">{player.id}</div>
+    
+    <div className="profiLogo2">
+    <img className="profileLogo2" src={player.image2} />
+    </div>
+    <img className="profiLogo" src={player.image} />
+    <h1 className="PlayerName">Atiysu Dev</h1>
+    <p className="noIdea">Personal</p>
+    <div className="level">
+    <svg className="levellogo" width="10" height="10" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 0L5.59808 1.5V4.5L3 6L0.401924 4.5V1.5L3 0Z" fill="#79F1E0"/></svg>
+    <p className="leveltext">12.Lvl</p>
+    </div>
+    
   </div>
 );
 
